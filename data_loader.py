efa18bc2884cb6358de84299ad8c09169fdf146e
nissank19 <fabiannissankastudy@gmail.com>
2025-11-22 15:57:52 +0100

    fixed from multiindex by flattening it

--- data_loader.py
+++ data_loader.py
@@ -7,16 +7,16 @@ class Data_loader:
         self.start=start
         self.end=end
 
-    def load (self):
-     df=yf.download(self.ticker,self.start,self.end)
-     df=df.reset_index() #makes a new column
-     df["Date"]=pd.to_datetime(df["Date"])
-     ##print(df.dtypes)
-     df=df.sort_values(by="Date") #sort by date
-     df = df.reset_index(drop=True) #the inital dataframe has not order
-     df.ffill() #to fill the mossing values with what was previous
-     df.rename(columns={"adj close":"adj_close"})
-     return df
+
+    def load(self):
+        df = yf.download(self.ticker, self.start, self.end, auto_adjust=True)
+        df = df.reset_index()  # make Date a column
+        # flatten MultiIndex columns
+        df.columns = ['_'.join(col).strip() if isinstance(col, tuple) else col for col in df.columns]
+        df.rename(columns={"Date_": "Date", "Close_AAPL": "adj_close"}, inplace=True)
+        df = df.sort_values(by="Date").reset_index(drop=True)
+        df = df.ffill()
+        return df
 
     def returns(self,df):
         df['return'] = df['adj_close'].pct_change() #checks the change
@@ -24,6 +24,10 @@ class Data_loader:
         df['log_return']=np.log((df["adj_close"]/df["adj_close"].shift(1)))
         df = df.dropna(subset=['return', 'log_return']) #drops first cuz both empty
         return df
+loader = Data_loader("AAPL")       # create instance
+df = loader.load()                 # download and clean data
+df = loader.returns(df)            # compute returns
+print(df.head())
 
 
 
